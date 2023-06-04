a8a692a4b66841e9ea1fc83c9da1058044008159
NubScript7 <112324399+NubScript7@users.noreply.github.com>
2023-06-04 15:50:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
